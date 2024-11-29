59c27d4d3ce66e6ca5736b9dd2a49b07cdad9c1c
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2024-11-29 15:45:32 -0500

    Time: 6 ms (80.52%), Space: 22.9 MB (9.01%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -23,18 +23,16 @@ public:
                 maxele = max(maxele,piles[i]);
         }
         int low =1,high = maxele;
-        int ans =maxele;
         int hours=0;
         while(low<=high){
             int mid = (low+high)/2;
             hours = requiredtime(piles,mid,h);
             if(hours <=h){
-                ans = min(ans,mid);
                 high = mid - 1;
             }else{
                 low = mid +1 ;
             }
         }
-        return ans;
+        return low;
     }
 };
