3818ac730e13d2b4fccf062920f132ffde627657
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2024-11-29 15:46:25 -0500

    Time: 6 ms (80.52%), Space: 22.9 MB (9.01%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas-optimised✨.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    int requiredtime(vector<int> &piles, int rate, int h){
+    int hours=0;
+        for(int i=0;i<piles.size();++i){
+            if(hours>h){
+                break;
+            }
+            if(piles[i]%rate==0){
+                hours += piles[i]/rate;
+            }else{
+                hours += (piles[i]/rate) +1;
+            }
+        }
+        return hours;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxele = 0;
+        for(int i=0;i<piles.size();++i){
+            if(piles[i]>maxele)
+                maxele = max(maxele,piles[i]);
+        }
+        int low =1,high = maxele;
+        int hours=0;
+        while(low<=high){
+            int mid = (low+high)/2;
+            hours = requiredtime(piles,mid,h);
+            if(hours <=h){
+                high = mid - 1;
+            }else{
+                low = mid +1 ;
+            }
+        }
+        return low;
+    }
+};
