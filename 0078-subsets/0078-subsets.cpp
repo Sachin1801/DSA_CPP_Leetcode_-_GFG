cf642778ea2d43d3b5765bf41673c9e2bb35f4a5
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2024-12-27 18:49:37 -0500

    Time: 0 ms (100%), Space: 9.8 MB (62.73%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,21 @@
 class Solution {
-
+    void find(int i,vector<int> &nums,vector<int> &curr,vector<vector<int>> &ans)
+    {
+        ans.push_back(curr);
+        if(i==nums.size())
+        return ;
+        for(int idx=i;i<nums.size();i++)
+        {
+            curr.push_back(nums[i]);
+            find(i+1,nums,curr,ans);
+            curr.pop_back();
+        }
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans ;
-        int n = nums.size();
-        int limit = pow(2,n) - 1;
-        for(int num = 0; num <=limit; num++){
-            vector<int> substring;
-            for(int i=0;i<n;i++){
-                if(num&(1<<i))
-                    substring.push_back(nums[i]);
-            }
-            ans.push_back(substring);
-        }
+        vector<vector<int>> ans;
+        vector<int> curr;
+        find(0,nums,curr,ans);
         return ans;
     }
 };
