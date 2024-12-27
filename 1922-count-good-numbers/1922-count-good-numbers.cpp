30fcb99cec41d3aa3b4a5f0c967a5b197da841f8
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2024-12-27 14:08:08 -0500

    Time: 0 ms (100%), Space: 7.9 MB (17.64%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    long long myPow(long long x, long long n) {
+        long long ans = 1;
+        while(n){
+            if(n&1) ans = (ans*x)%1000000007;
+            x = (x*x)%1000000007;
+            n = n>>1;
+        }  
+        return ans;
+    }
+
+public:
+    int countGoodNumbers(long long n) {
+        long long oddmultiple ;
+        long long evenmultiple ;
+        if(n&1){
+            oddmultiple = myPow(5,(n/2)+1);
+            evenmultiple = myPow(4,(n/2)) ;
+        }else{
+            oddmultiple = myPow(5,(n/2)) ;
+            evenmultiple = myPow(4,(n/2));
+        }
+        return ((oddmultiple * evenmultiple)%1000000007);
+    }
+};
