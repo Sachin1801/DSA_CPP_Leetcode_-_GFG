063f6d1e907da4ee35a74e61a5990f12915079df
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-01-29 01:51:41 -0500

    Time: 22 ms (29.56%), Space: 20.6 MB (13.67%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,57 +1,61 @@
 #include <vector>
-#include <limits>
-#include <algorithm>
-
+#include <queue>
+#include <climits>
 using namespace std;
 
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        // Initialize the distance matrix with infinity
-        vector<vector<int>> dist(n, vector<int>(n, numeric_limits<int>::max()));
-        
-        // Distance to itself is 0
-        for (int i = 0; i < n; ++i) {
-            dist[i][i] = 0;
+        // Create adjacency list
+        vector<vector<pair<int, int>>> Adj(n);
+        for (auto& edge : edges) {
+            Adj[edge[0]].push_back({edge[1], edge[2]});
+            Adj[edge[1]].push_back({edge[0], edge[2]});
         }
-        
-        // Populate the distance matrix with the given edges
-        for (const auto& edge : edges) {
-            int u = edge[0], v = edge[1], w = edge[2];
-            dist[u][v] = w;
-            dist[v][u] = w;
-        }
-        
-        // Floyd-Warshall algorithm
-        for (int k = 0; k < n; ++k) {
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    if (dist[i][k] != numeric_limits<int>::max() && dist[k][j] != numeric_limits<int>::max()) {
-                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+
+        auto dijkstra = [&](int src) {
+            vector<int> dist(n, INT_MAX);
+            dist[src] = 0;
+            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+            pq.push({0, src}); // {distance, node}
+
+            while (!pq.empty()) {
+                auto [currDist, currNode] = pq.top();
+                pq.pop();
+
+                if (currDist > dist[currNode]) continue;
+
+                for (auto& [neighbor, weight] : Adj[currNode]) {
+                    if (dist[currNode] + weight < dist[neighbor]) {
+                        dist[neighbor] = dist[currNode] + weight;
+                        pq.push({dist[neighbor], neighbor});
                     }
                 }
             }
-        }
-        
-        // Find the city with the smallest number of reachable cities
-        // and if there is a tie, choose the city with the greatest number.
-        int minReachableCities = numeric_limits<int>::max();
-        int bestCity = -1;
-        
-        for (int i = 0; i < n; ++i) {
-            int reachableCities = 0;
-            for (int j = 0; j < n; ++j) {
-                if (dist[i][j] <= distanceThreshold) {
-                    reachableCities++;
+            return dist;
+        };
+
+        // Find the city with the smallest number of neighbors within the distance threshold
+        int resultCity = -1, minNeighborCount = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
+            vector<int> dist = dijkstra(i);
+            int neighborCount = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (i != j && dist[j] <= distanceThreshold) {
+                    neighborCount++;
                 }
             }
-            
-            if (reachableCities <= minReachableCities) {
-                minReachableCities = reachableCities;
-                bestCity = i;
+
+            // Update the result based on the problem statement
+            if (neighborCount < minNeighborCount || 
+                (neighborCount == minNeighborCount && i > resultCity)) {
+                minNeighborCount = neighborCount;
+                resultCity = i;
             }
         }
-        
-        return bestCity;
+
+        return resultCity;
     }
 };
