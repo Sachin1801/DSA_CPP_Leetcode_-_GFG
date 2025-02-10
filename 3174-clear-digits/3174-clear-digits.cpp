3b35ca6c048a60b2243be8a0e2c26ef4560f67cc
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-02-11 02:38:05 +0530

    Time: 0 ms (100%), Space: 9 MB (41.58%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        deque<char> q;
+        for(int i=0;i<s.length();i++){
+            if(isdigit(s[i])){
+                q.pop_back();
+            }
+            if(isalpha(s[i])){
+                q.push_back(s[i]);
+            }
+        }
+        string ans = "";
+        while(!q.empty()){
+            ans += q.front();
+            q.pop_front();
+        }
+        return ans;
+    }
+};
