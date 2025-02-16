36f98c7eab74b93721ed785a020006e602bee864
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-02-16 17:41:36 -0500

    Time: 3 ms (20.43%), Space: 9.1 MB (15.65%) - LeetHub

--- 1718-construct-the-lexicographically-largest-valid-sequence/1718-construct-the-lexicographically-largest-valid-sequence.cpp
+++ 1718-construct-the-lexicographically-largest-valid-sequence/1718-construct-the-lexicographically-largest-valid-sequence.cpp
@@ -1,41 +1,50 @@
 class Solution {
-private:
-    bool backtrack(int i, vector<int>& ans, vector<int> &marked, int n) {
-        if (i == ans.size())
-            return true;
-
-        if (ans[i] != 0)
-            return backtrack(i + 1, ans, marked, n);
-
-        for (int j = n; j > 0; j--) {
-            // Validation check
-            if (marked[j]) continue;
-
-            if (j > 1 && (i + j >= ans.size() || ans[i + j] != 0))
-                continue;
-
-            // Try placing j
-            marked[j] = 1;
-            ans[i] = j;
-            if (j > 1) ans[i + j] = j;
-
-            // Recursive step
-            if (backtrack(i + 1, ans, marked, n))
-                return true;
-
-            // Backtrack
-            marked[j] = 0;
-            ans[i] = 0;
-            if (j > 1) ans[i + j] = 0;
-        }
-        return false;
-    }
-
 public:
+    void solve(int index, vector<int>& arr, map<int,bool>& m,int n,bool& found){
+        if(index==arr.size()){ //we reach the end ...all numbers    placed
+            found = true;
+            return;
+        }
+        if(arr[index]!=-1){ //No space at this index...move ahead
+            solve(index+1,arr,m,n,found);
+            return;
+        }
+        // the index is vacant
+        for(int i=n; i>=1; i--){ 
+            if(m[i]==false){ //To check whether we have placed this number before or not
+                if(i!=1 && (index+i)<arr.size() && arr[index+i]==-1){
+                    arr[index]=arr[index+i]=i;
+                    m[i]= true;
+                    solve(index+1,arr,m,n,found);
+                    if(found==true){ //EARLY STOPPING ... the catch!
+                        return;  
+                    }
+                    arr[index]=arr[index+i]=-1; //backtrack
+                }
+                else if(i==1){
+                    arr[index] = 1;
+                    m[i]= true;
+                    solve(index+1,arr,m,n,found);
+                    if(found==true){ //EARLY STOPPING.... the catch
+                        return;
+                    }
+                    arr[index] = -1; //backtrack
+                }
+                m[i] = false; //backtrack
+            }
+        }
+        return;
+    }
     vector<int> constructDistancedSequence(int n) {
-        vector<int> ans(2 * n - 1, 0);
-        vector<int> marked(n + 1, 0); // Replace unordered_set with a vector
-        backtrack(0, ans, marked, n);
-        return ans;
+        int arrSize = 2*n-1; // figure this out!
+        vector<int>arr(arrSize,-1);
+        map<int,bool>m;
+        for(int i = 1; i<=n; i++){
+            m.insert({i,false});
+        }
+        int index = 0;
+        bool found = false; // a flag variable we use for early stopping
+        solve(index,arr,m,n,found);
+        return arr;
     }
 };
