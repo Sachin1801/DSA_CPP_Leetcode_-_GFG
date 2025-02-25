3e96e240a95ade4f125148af2efae94e4431e9c8
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-02-25 13:14:04 -0500

    Time: 20 ms (14.9%), Space: 124.1 MB (11.54%) - LeetHub

---
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sumoptimised.cpp
@@ -0,0 +1,24 @@
+#define MOD 1000000007
+
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        int n = arr.size();
+        int ans =0;
+        int oddcnt = 0;
+        int evencnt =0;
+        vector<int> prefixsum;
+        for(int i=0;i<n;i++){
+            if(i==0) prefixsum.push_back(arr[i]);
+            else prefixsum.push_back(prefixsum[i-1]+arr[i]);
+            if(prefixsum[i]&1){
+                oddcnt++;
+                ans = (ans%MOD + 1 + evencnt%MOD)%MOD;
+            }else{
+                evencnt++;
+                ans += oddcnt;
+            }
+        }
+        return ans;
+    }
+};
