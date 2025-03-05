cdc3731426ed7d05f131340982ae2ffc0ccb9a9f
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-03-04 19:56:30 -0500

    Time: 0 ms (100%), Space: 7.9 MB (61.42%) - LeetHub

---
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long long ans=1;
+        if(n==1) return ans;
+        for(int i=1;i<n;i++){
+            ans += (4*i); 
+        }
+        return ans;
+    }
+};
