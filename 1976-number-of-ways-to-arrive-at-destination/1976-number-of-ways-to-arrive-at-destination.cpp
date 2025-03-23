8fd7e7675e84be4a9812d7f16e8cfd9df882004a
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-03-23 14:30:41 -0400

    Time: 0 ms (100%), Space: 35.1 MB (78.75%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,48 +1,49 @@
-#define MOD 1000000007
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<long long> ways(n, 0); // Use long long to avoid overflow
-        vector<long long> dist(n, LLONG_MAX); // Use long long for distances
-        vector<vector<pair<int, int>>> Adj(n);
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>,
-                       greater<pair<long long, int>>> pq;
+        const long long MOD = 1000000007;
 
-        // Build adjacency list
-        for (auto road : roads) {
-            Adj[road[0]].push_back({road[1], road[2]});
-            Adj[road[1]].push_back({road[0], road[2]});
+        // Adjacency list: for each node, store {neighbor, weight}
+        vector<vector<pair<int,int>>> graph(n);
+        for (auto &r : roads) {
+            int u = r[0], v = r[1], w = r[2];
+            graph[u].push_back({v, w});
+            graph[v].push_back({u, w});
         }
 
-        // Initialize priority queue and distance/ways arrays
-        pq.push({0, 0});
+        // dist[i]: shortest distance from 0 to i
+        // ways[i]: number of shortest paths from 0 to i
+        vector<long long> dist(n, LLONG_MAX), ways(n, 0);
         dist[0] = 0;
         ways[0] = 1;
 
+        // Min-heap of {distance, node}
+        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<>> pq;
+        pq.push({0, 0});
+
         while (!pq.empty()) {
-            auto top = pq.top();
-            long long currdst = top.first;
-            int currnode = top.second;
+            auto [currDist, u] = pq.top();
             pq.pop();
 
-            // If the current distance is greater than the known distance, skip
-            if (currdst > dist[currnode]) continue;
+            // If this distance is stale, ignore
+            if (currDist > dist[u]) continue;
 
-            for (auto nbr : Adj[currnode]) {
-                int node = nbr.first;
-                long long wt = nbr.second;
-
-                if (dist[node] > dist[currnode] + wt) {
-                    dist[node] = dist[currnode] + wt;
-                    pq.push({dist[node], node});
-                    ways[node] = ways[currnode] % MOD;
-                } else if (dist[node] == dist[currnode] + wt) {
-                    ways[node] = (ways[node] + ways[currnode]) % MOD;
+            // Relax edges
+            for (auto &[v, w] : graph[u]) {
+                long long newDist = dist[u] + w;
+                if (newDist < dist[v]) {
+                    dist[v] = newDist;
+                    ways[v] = ways[u]; // inherit the number of ways
+                    pq.push({dist[v], v});
+                } else if (newDist == dist[v]) {
+                    ways[v] = (ways[v] + ways[u]) % MOD;
                 }
             }
         }
 
-        return ways[n - 1] % MOD;
+        return ways[n-1] % MOD;
     }
 };
