3b9759ffd0a0094d86b4318a8c5f3827e912b705
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-04-01 18:04:00 -0400

    Time: 6 ms (58.92%), Space: 127.4 MB (41.94%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    long long solve(vector<long long> &dp, vector<vector<int>>& questions, int i) {
+        if (i >= questions.size())
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+
+        long long pick = questions[i][0] + solve(dp, questions, i + questions[i][1] + 1);
+        
+        long long skip = solve(dp,questions,i+1);
+
+        return dp[i]= max(pick,skip);
+
+        // not pick
+    }
+
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        vector<long long> dp(n,-1);
+        
+        long long ans  = solve(dp, questions, 0);
+        return dp[0];
+    }
+};
