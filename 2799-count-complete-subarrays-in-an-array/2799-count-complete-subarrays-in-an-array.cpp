2c5d37a9ca8bd781ef497ea8dc31e4eaec8f1534
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-04-24 12:37:03 -0400

    Time: 0 ms (100%), Space: 48.9 MB (25.57%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
-        int left =0,right=0;
-        int curr;
-        int n = nums.size();
-        unordered_map<int,int> freq;
+        int l =0, r=0, n= nums.size(), ans =0;
+        bitset<2001> ht;
+        bitset<2001> curr;
         for(int i=0;i<n;i++){
-            freq[nums[i]]++;
+            if(!ht.test(nums[i])) ht.set(nums[i]);
         }
-        int tot_dist = freq.size();
-        freq.clear();
-        int ans = 0;
-        while(right < n){
-            freq[nums[right]]++;
-            if(freq.size()==tot_dist){
-                unordered_map<int,int> temp(freq.begin(),freq.end());
-                curr = left;
-                while(temp.size()==tot_dist){
-                    ans++;
-                    temp[nums[curr]]--;
-                    if(temp[nums[curr]]==0) temp.erase(nums[curr]);
-                    curr++;
-                }
+        vector<int> ht2(2001,0);
+        while(r<n){
+            curr.set(nums[r]);
+            ht2[nums[r]]++;
+            while(curr == ht && l <= r){
+                ans += (n-r);
+
+                ht2[nums[l]]--;
+                if(ht2[nums[l]]==0) curr.reset(nums[l]);
+                l++;
             }
-            right++;
+            r++;
         }
         return ans;
     }
