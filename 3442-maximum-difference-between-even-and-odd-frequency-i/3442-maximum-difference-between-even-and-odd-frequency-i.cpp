93585a157029d1f728fcd60432fd5bdb2d821179
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-06-10 19:01:22 -0400

    Time: 0 ms (100%), Space: 9.2 MB (94.34%) - LeetHub

---
+++ 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxDifference(string s) {
+        vector<int> freq(26,0);
+
+        for(int i=0;i<s.length();i++){
+            freq[ s[i] -'a']++;
+        }
+
+        int max_freq = 0, min_freq=INT_MAX;
+
+
+        for(auto& f: freq){
+            if(f==0) continue;
+            if(f&1){
+                max_freq = max(max_freq,f);
+            }else{
+                min_freq = min(min_freq,f);
+            }
+        }
+        return max_freq-min_freq;
+    }
+};
