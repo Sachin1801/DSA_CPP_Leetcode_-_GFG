5a980cba1c6971fda6df54b6d4e1fae928cafca3
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:16:52 -0500

    Create 2277-CountEqualAndDivisiblePairsInAnArray

---
+++ 2277-CountEqualAndDivisiblePairsInAnArray/2277-CountEqualAndDivisiblePairsInAnArray.cpp
@@ -0,0 +1,17 @@
+// Last updated: 12/14/2025, 4:16:52 PM
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        unordered_map<int,vector<int>> idxList;
+        int ans =0;
+        for(int i=0;i<nums.size();i++){
+            if(idxList[nums[i]].size()!=0){
+                for(auto num: idxList[nums[i]]){
+                    if((i*num)%k==0) ans++;
+                }
+            }
+            idxList[nums[i]].push_back(i);
+        }    
+        return ans;
+    }
+};
