4ed5d38ffe6dd0a3abd056aa85aceac3a74fc922
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:15:31 -0500

    Create 3621-MinimumOperationsToMakeArrayValuesEqualToK

---
+++ 3621-MinimumOperationsToMakeArrayValuesEqualToK/3621-MinimumOperationsToMakeArrayValuesEqualToK.cpp
@@ -0,0 +1,15 @@
+// Last updated: 12/14/2025, 4:15:30 PM
+class Solution {
+    public:
+        int minOperations(vector<int>& nums, int k) {
+            int mini = *min_element(nums.begin(), nums.end());
+            if (mini < k) return -1;
+
+            bitset<100001> st;
+            for (int i : nums) 
+                if (i > k) st.set(i);
+                
+            return st.count();
+        }
+    };
+    
