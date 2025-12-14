8fee20427eeb84d1babdc281edb2440506879de8
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:17:07 -0500

    Create 1993-SumOfAllSubsetXorTotals

---
+++ 1993-SumOfAllSubsetXorTotals/1993-SumOfAllSubsetXorTotals.cpp
@@ -0,0 +1,23 @@
+// Last updated: 12/14/2025, 4:17:06 PM
+class Solution {
+private:
+    int ans =0;
+
+    void solve(vector<int> &nums, int i, int currxor){
+        if(i>=nums.size()){
+            ans += currxor;
+            return;
+        }
+
+        //option 1: not pick
+        solve(nums,i+1,currxor);
+
+        //option 2: pick
+        solve(nums,i+1,currxor^nums[i]);
+    }
+public:
+    int subsetXORSum(vector<int>& nums) {
+        solve(nums,0,0);
+        return ans;
+    }
+};
