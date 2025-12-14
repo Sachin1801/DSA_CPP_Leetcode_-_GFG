94f1234935d7012ae8f2e43c452e23e584d9a374
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:15:50 -0500

    Create 2856-CountCompleteSubarraysInAnArray

---
+++ 2856-CountCompleteSubarraysInAnArray/2856-CountCompleteSubarraysInAnArray.cpp
@@ -0,0 +1,26 @@
+// Last updated: 12/14/2025, 4:15:50 PM
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        int l =0, r=0, n= nums.size(), ans =0;
+        bitset<2001> ht;
+        bitset<2001> curr;
+        for(int i=0;i<n;i++){
+            if(!ht.test(nums[i])) ht.set(nums[i]);
+        }
+        vector<int> ht2(2001,0);
+        while(r<n){
+            curr.set(nums[r]);
+            ht2[nums[r]]++;
+            while(curr == ht && l <= r){
+                ans += (n-r);
+
+                ht2[nums[l]]--;
+                if(ht2[nums[l]]==0) curr.reset(nums[l]);
+                l++;
+            }
+            r++;
+        }
+        return ans;
+    }
+};
