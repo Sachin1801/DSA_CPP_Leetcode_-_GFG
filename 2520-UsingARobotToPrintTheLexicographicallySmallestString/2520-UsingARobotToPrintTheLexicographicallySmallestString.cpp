3606387db11579b4c848a056d3dfc6dc65c224a4
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:16:35 -0500

    Create 2520-UsingARobotToPrintTheLexicographicallySmallestString

---
+++ 2520-UsingARobotToPrintTheLexicographicallySmallestString/2520-UsingARobotToPrintTheLexicographicallySmallestString.cpp
@@ -0,0 +1,36 @@
+// Last updated: 12/14/2025, 4:16:35 PM
+class Solution {
+public:
+    string robotWithString(string s) {
+        map<char,vector<int>> m;
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            m[s[i]].push_back(i);
+        }
+        vector<bool> visited(26,0);
+        int i = 0;
+        stack<char> st;
+        string ans ="";
+        for(auto &pair: m){
+            if(i == n) break;
+            char ch = pair.first;
+            visited[ch-'a']=true;
+            vector<int> &v = pair.second;
+            for(int j = 0; j<v.size();j++){
+                while(i < n && i<=v[j]){
+                    st.push(s[i]);
+                    i++;
+                }
+                while(!st.empty() && (st.top() == ch || visited[st.top()-'a']== true)){
+                    ans += st.top();
+                    st.pop();
+                }
+            }
+        }
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
