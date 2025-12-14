226c02b762808d90b20ba42a30dee273c67adbc9
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:16:43 -0500

    Create 2456-ConstructSmallestNumberFromDiString

---
+++ 2456-ConstructSmallestNumberFromDiString/2456-ConstructSmallestNumberFromDiString.cpp
@@ -0,0 +1,21 @@
+// Last updated: 12/14/2025, 4:16:42 PM
+class Solution {
+public:
+    string smallestNumber(string pattern) {
+        int n = pattern.size();
+        stack<int> st;
+        string ans = "";
+        for(int i=1;i<=n+1;i++){
+            st.push(i);
+            char ch = pattern[i-1];
+
+            if(i==n+1 or ch =='I'){
+                while(!st.empty()){
+                    ans += char(('0' + st.top()));
+                    st.pop();
+                }
+            }
+        }
+        return ans;
+    }
+};
