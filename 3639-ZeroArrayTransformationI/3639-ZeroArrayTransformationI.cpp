8f012102abb0b2dbde75a1fec2fb9515d2950926
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:15:26 -0500

    Create 3639-ZeroArrayTransformationI

---
+++ 3639-ZeroArrayTransformationI/3639-ZeroArrayTransformationI.cpp
@@ -0,0 +1,25 @@
+// Last updated: 12/14/2025, 4:15:26 PM
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> deltaArray(nums.size() + 1, 0);
+        for (const auto& query : queries) {
+            int left = query[0];
+            int right = query[1];
+            deltaArray[left] += 1;
+            deltaArray[right + 1] -= 1;
+        }
+        vector<int> operationCounts;
+        int currentOperations = 0;
+        for (int delta : deltaArray) {
+            currentOperations += delta;
+            operationCounts.push_back(currentOperations);
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (operationCounts[i] < nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
