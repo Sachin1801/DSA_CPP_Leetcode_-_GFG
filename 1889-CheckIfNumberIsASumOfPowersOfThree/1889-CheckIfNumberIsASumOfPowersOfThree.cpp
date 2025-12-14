fea4d105ae34c941f2c846f1b58182bfb7c54f0d
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:17:10 -0500

    Create 1889-CheckIfNumberIsASumOfPowersOfThree

---
+++ 1889-CheckIfNumberIsASumOfPowersOfThree/1889-CheckIfNumberIsASumOfPowersOfThree.cpp
@@ -0,0 +1,13 @@
+// Last updated: 12/14/2025, 4:17:10 PM
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+};
