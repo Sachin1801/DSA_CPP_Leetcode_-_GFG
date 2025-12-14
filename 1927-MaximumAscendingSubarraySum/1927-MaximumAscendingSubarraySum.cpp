3f183a48e6eb654655fbdb22de90b4ea845bc131
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:17:08 -0500

    Create 1927-MaximumAscendingSubarraySum

---
+++ 1927-MaximumAscendingSubarraySum/1927-MaximumAscendingSubarraySum.cpp
@@ -0,0 +1,24 @@
+// Last updated: 12/14/2025, 4:17:08 PM
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        if(nums.size()==1)
+            return nums[0];
+        
+        int msum=0;
+        int csum=0;
+        int n = nums.size();
+        int i ;
+        for(i=0;i<n-1;i++){
+            while(i<n-1 and nums[i]<nums[i+1]){
+                csum+=nums[i];
+                i++;
+            }
+            csum += nums[i];
+            if(csum > msum )
+                msum = csum;
+            csum=0;
+        }
+        return msum;
+    }
+};
