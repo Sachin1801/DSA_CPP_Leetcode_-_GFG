e791a501709dd21951006d91eb94bbfcef61e057
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:15:37 -0500

    Create 3455-MinimumLengthOfStringAfterOperations

---
+++ 3455-MinimumLengthOfStringAfterOperations/3455-MinimumLengthOfStringAfterOperations.cpp
@@ -0,0 +1,12 @@
+// Last updated: 12/14/2025, 4:15:36 PM
+class Solution {
+public:
+    int minimumLength(string s) {
+        ios::sync_with_stdio(0); cin.tie(0);
+        vector<int>ct(26);
+        for(char c:s) ct[c-'a']++;
+        int res = 0;
+        for(int i=0;i<26;i++) res += (ct[i]%2? 1 : (ct[i]? 2 : 0));
+        return res;
+    }
+};
