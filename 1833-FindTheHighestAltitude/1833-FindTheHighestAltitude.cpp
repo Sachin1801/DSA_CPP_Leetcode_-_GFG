e8f003a28543622da18db7dfe42cc3c6e777baba
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:17:14 -0500

    Create 1833-FindTheHighestAltitude

---
+++ 1833-FindTheHighestAltitude/1833-FindTheHighestAltitude.cpp
@@ -0,0 +1,17 @@
+// Last updated: 12/14/2025, 4:17:14 PM
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int csum=0;
+        int msum=INT_MIN;
+        int n = gain.size();
+        vector<int> prefix(n+1);
+        prefix[0]=0;
+        for(int i=0;i<n;i++){
+            if(msum < csum )
+                msum = max(csum,msum);
+            csum= csum +gain[i];
+        }
+        return max(msum,csum);
+    }
+};
