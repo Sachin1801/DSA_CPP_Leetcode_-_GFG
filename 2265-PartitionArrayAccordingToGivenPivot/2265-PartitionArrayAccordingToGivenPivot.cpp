b47f8e9092e6b7cf7f7d4f23382f271265b7d188
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:16:53 -0500

    Create 2265-PartitionArrayAccordingToGivenPivot

---
+++ 2265-PartitionArrayAccordingToGivenPivot/2265-PartitionArrayAccordingToGivenPivot.cpp
@@ -0,0 +1,29 @@
+// Last updated: 12/14/2025, 4:16:53 PM
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        unordered_map<char,vector<int>> m;
+        m.reserve(3);
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]< pivot)
+                m['l'].push_back(nums[i]);
+            else if(nums[i]==pivot)
+                m['e'].push_back(nums[i]);
+            else
+                m['g'].push_back(nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            for(int elem: m['l']){
+                nums[i++] = elem;
+            }
+            for(auto elem: m['e']){
+                nums[i++] = elem;
+            }
+            for(auto elem: m['g']){
+                nums[i++] = elem;
+            }
+        }
+        return nums;
+    }
+};
