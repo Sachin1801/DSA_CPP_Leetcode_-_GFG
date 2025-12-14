c297d2b2839383c09f20154265f788e0f13b0ce3
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-14 16:15:45 -0500

    Create 3227-FindMissingAndRepeatedValues

---
+++ 3227-FindMissingAndRepeatedValues/3227-FindMissingAndRepeatedValues.cpp
@@ -0,0 +1,22 @@
+// Last updated: 12/14/2025, 4:15:44 PM
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        long long s = n*n;
+        vector<long long> val(s+1,-1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                val[grid[i][j]]++;
+            }
+        }
+        vector<int> ans(2);
+        for(int i=1;i<val.size();i++){
+            if(val[i]==-1){
+                ans[1]=i;
+            }
+            if(val[i]==1) ans[0]=i;
+        }
+        return ans;
+    }
+};
