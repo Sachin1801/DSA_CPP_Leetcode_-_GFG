ac075d72c25e1b7143d042e60307b1f7feb46220
Sachin Adlakha <87144268+Sachin1801@users.noreply.github.com>
2025-12-16 16:54:30 -0500

    Time: 4 ms (81.85%), Space: 13.1 MB (57.93%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    #define vii vector<vector<int> >
-    
-    int edit_distance(string &s1, string &s2,int n, int m,vii &dp)
-{
-	if(n==0) return dp[n][m] = m;
-	if(!m) return dp[n][m] =n; // !m is same as m==0
-    
-    if(dp[n][m]!=-1) return dp[n][m];
-	if(s1[n-1]!=s2[m-1])
-	{
-		return dp[n][m] = min(edit_distance(s1,s2,n-1,m,dp),
-			min(edit_distance(s1,s2,n,m-1,dp),
-				edit_distance(s1,s2,n-1,m-1,dp))) + 1;
-	} 
-	else{
-		return dp[n][m] = edit_distance(s1,s2,n-1,m-1,dp);
-	}
-}
-    
+
+    int f(int i, int j, string &word1, string &word2, vector<vector<int>> &dp){
+        if(j < 0 && i>=0) return i+1;
+        if(i < 0 && j >=0) return j+1;
+        if(i < 0 && j < 0) return 0;
+
+        if(dp[i][j]!= -1) return dp[i][j];
+        //case 1 : if the current char match
+        int total = 1e9;
+        if(word1[i]==word2[j]){
+            total = f(i-1,j-1,word1,word2,dp);
+        }else{
+            //case 1: we can delete and move on 
+            //case 2: we can insert and move on 
+            //case 3: we can replace and move on
+            total = 1 + min ( f(i-1,j,word1,word2,dp),min( f(i,j-1,word1,word2,dp), f(i-1,j-1,word1,word2,dp)));
+        }
+        return dp[i][j] = total;
+    }
+
     int minDistance(string word1, string word2) {
-        int n= word1.length();
-        int n2= word2.length();
-        vii dp(n+1,vector<int>(n2+1,-1));
-        return edit_distance(word1,word2,n,n2,dp);
+        int n = word1.size();
+        int m = word2.size();
+
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+
+        return f(n-1,m-1,word1,word2,dp);
     }
 };
